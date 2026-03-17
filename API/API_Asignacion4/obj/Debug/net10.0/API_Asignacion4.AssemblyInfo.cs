b20eccc749e4d959379380de8a323c987fbfd5d9
sleftsito <tomasuniversitario683@gmail.com>
2026-03-17 03:17:40 -0300

    Todo funciona en la laptop

--- API/API_Asignacion4/obj/Debug/net10.0/API_Asignacion4.AssemblyInfo.cs
+++ API/API_Asignacion4/obj/Debug/net10.0/API_Asignacion4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Asignacion4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5016744aa800f0a2b3bd54ed044e7fb4d24c9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Asignacion4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Asignacion4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
